dd02023038d70f7b49a3f19475b8fa6059a21fe2
Popa Andreea <andreea_popa04@yahoo.com>
2022-11-24 15:21:31 +0200

    register user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './models/user';
 import { AccountService } from './_services/account.service';
@@ -10,27 +9,17 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating app';
-  users: any;
 
   constructor(
-    private http: HttpClient,
     private accountService: AccountService
   ) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-    });
   }
 }
